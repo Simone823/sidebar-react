86bdacedefc6a9a0e95798a0e893ab51287c5f61
Simone823 <simone.daglio823@outlook.it>
2022-10-25 23:16:59 +0200

    use context header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import React, {useState, createContext} from 'react';
 // import sidebar
 import Sidebar from './Sidebar';
 
+// header context
+export const HeaderContext = createContext();
+
 function Header() {
     // isOpen menu
     const [isOPenMenu, setIsOpenMenu] = useState(false);  
@@ -21,53 +24,55 @@ function Header() {
 
     return (
         // header
-        <header>
-            <div className='container'>
-                <div className='wrapper'>
+        <HeaderContext.Provider value={{isOPenMenu, closeMenu}}>
+            <header>
+                <div className='container'>
+                    <div className='wrapper'>
 
-                    {/* brand */}
-                    <a href='/' className='nav-brand'>NavBar</a>
+                        {/* brand */}
+                        <a href='/' className='nav-brand'>NavBar</a>
 
-                    {/* links */}
-                    <ul className='list-links-menu'>
-                        <li>
-                            <a href='/' className='nav-link'>Home</a>
-                        </li>
-                        <li>
-                            <a href='/' className='nav-link'>Chi Siamo</a>
-                        </li>
-                        <li>
-                            <a href='/' className='nav-link'>Progetti</a>
-                        </li>
-                        <li>
-                            <a href='/' className='nav-link'>Contattaci</a>
-                        </li>
-                    </ul>
+                        {/* links */}
+                        <ul className='list-links-menu'>
+                            <li>
+                                <a href='/' className='nav-link'>Home</a>
+                            </li>
+                            <li>
+                                <a href='/' className='nav-link'>Chi Siamo</a>
+                            </li>
+                            <li>
+                                <a href='/' className='nav-link'>Progetti</a>
+                            </li>
+                            <li>
+                                <a href='/' className='nav-link'>Contattaci</a>
+                            </li>
+                        </ul>
 
-                    {/* social list */}
-                    <ul className='social-list'>
-                        <li>
-                            <a href='/'><i className="fab fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href='/'><i className="fab fa-facebook"></i></a>
-                        </li>
-                        <li>
-                            <a href='/'><i className="fab fa-linkedin-in"></i></a>
-                        </li>
-                    </ul>
+                        {/* social list */}
+                        <ul className='social-list'>
+                            <li>
+                                <a href='/'><i className="fab fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href='/'><i className="fab fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href='/'><i className="fab fa-linkedin-in"></i></a>
+                            </li>
+                        </ul>
 
-                    {/* btn hamburger menu */}
-                    <button onClick={openMenu()} type='button' className='btn-menu'>
-                        <i className="fas fa-bars"></i>
-                    </button>
+                        {/* btn hamburger menu */}
+                        <button onClick={openMenu()} type='button' className='btn-menu'>
+                            <i className="fas fa-bars"></i>
+                        </button>
 
-                    {/* sidebar */}
-                    <Sidebar/>
+                        {/* sidebar */}
+                        <Sidebar/>
 
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </HeaderContext.Provider>
     )
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,54 +1,60 @@
-import React from 'react'
+import React, {useContext} from 'react';
+
+// import header context
+import {HeaderContext} from './Header';
 
 function Sidebar() {
 
-  return (
-    <div className='sidebar'>
-        <div className='container'>
+    // info header context
+    const {isOPenMenu, closeMenu} = useContext(HeaderContext);
 
-            {/* header */}
-            <div className='header-sidebar'>
-                {/* brand */}
-                <a href='/' className='nav-brand'>Sidebar</a>
+    return (
+        <div className={`sidebar ${isOPenMenu === true ? 'show' : 'hide'}`}>
+            <div className='container'>
 
-                {/* btn */}
-                <button type='button' className='btn-menu-close'>
-                    <i className="fas fa-times-circle"></i>
-                </button>
+                {/* header */}
+                <div className='header-sidebar'>
+                    {/* brand */}
+                    <a href='/' className='nav-brand'>Sidebar</a>
+
+                    {/* btn */}
+                    <button onClick={closeMenu()} type='button' className='btn-menu-close'>
+                        <i className="fas fa-times-circle"></i>
+                    </button>
+                </div>
+
+                {/* list links */}
+                <ul className='list-links'>
+                    <li>
+                        <a href='/' className='nav-link'>Home</a>
+                    </li>
+                    <li>
+                        <a href='/' className='nav-link'>Chi Siamo</a>
+                    </li>
+                    <li>
+                        <a href='/' className='nav-link'>Progetti</a>
+                    </li>
+                    <li>
+                        <a href='/' className='nav-link'>Contattaci</a>
+                    </li>
+                </ul>
+
+                {/* social list */}
+                <ul className='socials'>
+                    <li>
+                        <a href='/'><i className="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href='/'><i className="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href='/'><i className="fab fa-linkedin-in"></i></a>
+                    </li>
+                </ul>
+
             </div>
-
-            {/* list links */}
-            <ul className='list-links'>
-                <li>
-                    <a href='/' className='nav-link'>Home</a>
-                </li>
-                <li>
-                    <a href='/' className='nav-link'>Chi Siamo</a>
-                </li>
-                <li>
-                    <a href='/' className='nav-link'>Progetti</a>
-                </li>
-                <li>
-                    <a href='/' className='nav-link'>Contattaci</a>
-                </li>
-            </ul>
-
-            {/* social list */}
-            <ul className='socials'>
-                <li>
-                    <a href='/'><i className="fab fa-twitter"></i></a>
-                </li>
-                <li>
-                    <a href='/'><i className="fab fa-facebook"></i></a>
-                </li>
-                <li>
-                    <a href='/'><i className="fab fa-linkedin-in"></i></a>
-                </li>
-            </ul>
-
         </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
