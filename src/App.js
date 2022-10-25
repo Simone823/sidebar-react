63dcb269040a5b149a8348187cc6bcabd5a4a62f
Simone823 <simone.daglio823@outlook.it>
2022-10-25 17:33:06 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+// import header
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
+      {/* header */}
+      <Header/>
 
+      {/* main */}
+      <main>
+        
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Header() {
+  return (
+    // header
+    <header>
+        <div className='container'>
+            <div className='wrapper'>
+
+                {/* brand */}
+                <a href='/' className='nav-brand'>NavBar</a>
+
+                {/* links */}
+                <ul className='list-links-menu'>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                    <li>
+                        <a href='/'>Chi Siamo</a>
+                    </li>
+                    <li>
+                        <a href='/'>Progetti</a>
+                    </li>
+                    <li>
+                        <a href='/'>Contattaci</a>
+                    </li>
+                </ul>
+
+                {/* social list */}
+                <ul className='social-list'>
+                    <li>
+                        <a href='/'><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href='/'><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href='/'><i class="fab fa-linkedin-in"></i></a>
+                    </li>
+                </ul>
+
+                {/* btn hamburger menu */}
+                <button type='button' className='btn-menu'>
+                    <i class="fas fa-bars"></i>
+                </button>
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
